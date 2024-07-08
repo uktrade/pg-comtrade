667819b255eace693b7378e743c69d02569df9e5
Saisakul Chernbumroong <159020482+saisakul@users.noreply.github.com>
2024-07-08 14:21:47 +0100

    test: mock comtrade api

--- test_pg_comtrade.py
+++ test_pg_comtrade.py
@@ -2,30 +2,36 @@ import sqlalchemy
 from pg_comtrade import sync, query
 import pandas as pd
 import pytest
-
-@pytest.fixture
-def example_data():
-    return pd.DataFrame({'col1':['test1'], 'col2':['test2']})
+import requests_mock
     
 @pytest.fixture
 def schema():
-    return 'public'
+    return '_team_firebreak_pg'
     
 @pytest.fixture
 def table():
     return 'example'
 
+def comtrade_json():
+    df = pd.read_csv('./data/comtrade_example.txt', delimiter='\t')
+    return df.to_dict(orient='records')
 
-def test_sync(example_data, schema, table):
-    sql_engine = sqlalchemy.create_engine("postgresql://postgres:postgres@127.0.0.1:5432/")
-    with sql_engine.connect() as connection:
-        try:
-            sync(connection=connection, schema=schema,
-                table=table,
-                df=example_data,
-                if_exists='replace')
-        except Exception as e:
-            pytest.fail(f"Something is wrong with error: {repr(e)}")
 
-        df = query(connection, sql=f"SELECT * FROM {schema}.{table}")
-    assert  df.equals(example_data)
+def test_sync(schema, table):
+    # sql_engine = sqlalchemy.create_engine("postgresql://postgres:postgres@127.0.0.1:5432/")
+    sql_engine = sqlalchemy.create_engine("postgresql://")
+    
+    # This is setting up the "fake" API
+    with requests_mock.Mocker() as m:
+        url = 'https://comtradeapi.un.org/public/v1/getComtradeReleases'
+        m.get(url, json = comtrade_json())
+        with sql_engine.connect() as connection:
+            try:
+                sync(connection=connection, schema=schema,
+                    table=table,
+                    if_exists='replace')
+            except Exception as e:
+                pytest.fail(f"Something is wrong with error: {repr(e)}")
+
+            df = query(connection, sql=f"SELECT * FROM {schema}.{table}")
+    assert  df.equals(pd.read_csv('./data/comtrade_example.txt', delimiter='\t'))
